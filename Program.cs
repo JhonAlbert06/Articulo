fba2a81b02148b087cc11b126a8934335901e56f
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-10-05 19:37:01 -0400

    ver

--- Program.cs
+++ Program.cs
@@ -17,11 +17,11 @@ builder.Services.AddScoped<Contexto, Contexto>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
